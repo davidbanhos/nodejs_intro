eb9dd9ee1bc03082e6146d1055b8c814d2455cc0
David Saggese Banhos <david.banhos@gmail.com>
2015-10-17 15:41:17 +0800

    Implemented with emitters

--- hello.js
+++ hello.js
@@ -1,11 +1,17 @@
-# Simple server sample!
+//Simple server sample!
 
 var http = require('http');
 
-http.createServer(function(request,response) {
+var server = http.createServer();
+server.listen(8080);
+console.log('Listen to 8080!');
+
+server.on('request', function(request, response){
   response.writeHead(200);
   response.write('Hello there!');
   response.end();
-}).listen(8080);
+});
 
-console.log('Listen to 8080!');
+server.on('request', function(request, response) {
+  console.log('Request incoming...');
+});
